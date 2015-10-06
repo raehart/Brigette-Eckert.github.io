f364cd1add1963a0346896ea38cee19e17d0f17d
Brigette Eckert <brigette.eckert@live.com>
2015-10-05 19:06:32 -0700

    added dev post icon

--- web-apps/flickr-search/js/hints.js
+++
@@ -1,6 +0,0 @@
-$('form').submit(function(evt) {
-	//code here will run when sumbitted
-	evt.preventDefault(); // prevets page from reloading when sumbitted
-	var searchTerm = $("#search").val();
-
-});
