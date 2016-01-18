e97e521d67a664ea8d61d4edb8bfb01e6a50efcb
LawlietBlack <kenneth.black@live.com>
2016-01-18 02:05:24 -0800

    redesign partially complete, draft of navbar and portfolio done.

---
+++ js/app.js
@@ -0,0 +1,55 @@
+angular.module('PortfolioApp', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'MainController',
+			templateUrl: 'views/main.html'
+		}).when('/about', {
+			controller: 'MainController',
+			templateUrl: 'views/about.html'
+		});
+}])
+.controller('NavController', ['$scope', '$location', function($scope, $location) {
+	$scope.isActive = function(route) {
+    return route === $location.path();
+  };
+}])
+
+.controller('MainController', ['$scope', function($scope) {
+	$scope.projects = [{
+		title: 'Pac-NW Race Calendar',
+		img: 'img/projects/racecal.png',
+		description: 'A calendar that displays upcoming races in Oregon and Washington. Built using a jQuery, JSON, and AngularJS.',
+		github: 'https://github.com/Brigette-Eckert/PacNW-Race-Calendar',
+		link: 'http://git.bbeckert.com/race-calendar'
+		}, {
+		title: 'Pokedex',
+		img: 'img/projects/pokemon.png',
+		description: 'A guide to all the Pokemon in the Pokemon universe. Built with Angular JS and an external API.',
+		github: 'https://github.com/Brigette-Eckert/pokedex',
+		link: 'http://pokedex.lawlietblack.com'
+		}, {
+		title: 'Random Cat Fact',
+		img: 'img/projects/cats.png',
+		description: 'A random fact generator that displays cat facts when the button is clicked and allows user\'s to tweet fact.  Build with JavaScript, Twitter developer tools and external restful API.',
+		github: 'https://github.com/Brigette-Eckert/cat_facts',
+		link: 'http://git.bbeckert.com/cat_facts/'
+		}, {
+		title: 'Pomodoro Clock',
+		img: 'img/projects/clock.png',
+		description: 'A pomodoro clock that alerts user with sound when period is over, allows user to customize work and break periods. Built using JavaScript. Reversed engineered for use at Free Code Camp.',
+		github: 'https://github.com/Brigette-Eckert/pomodoro-clock',
+		link: 'http://git.bbeckert.com/pomodoro/'
+		}, {
+		title: 'Calculator',
+		img: 'img/projects/calc.png',
+		description: 'A calculator built using Javascript, jQuery and Sass. Reversed engineered as a student at Free Code Camp.',
+		github: 'https://github.com/Brigette-Eckert/calculator',
+		link: 'http://git.bbeckert.com/calculator/'
+	}]
+	$scope.getProjectStyle = function(img) { 
+		console.log(img)
+		return '{background-image: url("../' + img + '")}'
+	}
+}])
