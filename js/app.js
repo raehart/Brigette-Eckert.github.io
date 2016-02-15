4d59b8bef7af19513da19cc349afdc8608f32965
Brigette Eckert <brigette.eckert@live.com>
2016-02-14 21:23:19 -0800

    moved projects around and added wikipedia viewer

--- js/app.js
+++ js/app.js
@@ -18,18 +18,6 @@ angular.module('PortfolioApp', ['ngRoute'])
 
 .controller('MainController', ['$scope', function($scope) {
 	$scope.projects = [{
-		title: 'Pac-NW Race Calendar',
-		img: 'img/projects/racecal.png',
-		description: 'A calendar that displays upcoming races in Oregon and Washington. Built using a jQuery, JSON, and AngularJS.',
-		github: 'https://github.com/Brigette-Eckert/PacNW-Race-Calendar',
-		link: 'http://git.bbeckert.com/race-calendar'
-		}, {
-		title: 'Pokedex',
-		img: 'img/projects/pokemon.png',
-		description: 'A guide to all the Pokemon in the Pokemon universe. Built with Angular JS and an external API.',
-		github: 'https://github.com/Brigette-Eckert/pokedex',
-		link: 'http://pokedex.lawlietblack.com'
-		}, {
 		title: 'Random Cat Fact',
 		img: 'img/projects/cats.png',
 		description: 'A random fact generator that displays cat facts when the button is clicked and allows user\'s to tweet fact.  Build with JavaScript, Twitter developer tools and external restful API.',
@@ -53,5 +41,23 @@ angular.module('PortfolioApp', ['ngRoute'])
 		description: 'A weather displaying built using Javascript, Sass and Open Weather API (Bowser location must be on). Reversed engineered as a student at Free Code Camp.',
 		github: 'https://github.com/Brigette-Eckert/local-weather',
 		link: 'http://git.bbeckert.com/local-weather/'
-	}]
+	}, {
+		title: 'Pac-NW Race Calendar',
+		img: 'img/projects/racecal.png',
+		description: 'A calendar that displays upcoming races in Oregon and Washington. Built using a jQuery, JSON, and AngularJS.',
+		github: 'https://github.com/Brigette-Eckert/PacNW-Race-Calendar',
+		link: 'http://git.bbeckert.com/race-calendar'
+		}, {
+		title: 'Pokedex',
+		img: 'img/projects/pokemon.png',
+		description: 'A guide to all the Pokemon in the Pokemon universe. Built with Angular JS and an external API.',
+		github: 'https://github.com/Brigette-Eckert/pokedex',
+		link: 'http://pokedex.lawlietblack.com'
+		}, {
+		title: 'Wikipedia Viewer',
+		img: 'img/projects/wiki.png',
+		description: 'An electronic card catalog for Wikipedia. Allows user to search for a topic or view a randon article. Built using jquery, and media wiki API. Reversed engineered as a student at Free Code Camp..',
+		github: 'https://github.com/Brigette-Eckert/wikipedia_viewer',
+		link: 'http://git.bbeckert.com/race-calendar/wikipedia_viewer'
+		}]
 }])
